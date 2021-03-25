695295ee97e3d6be6cbf860f47dc8996fec34523
Amal Biju <amalbiju99@gmail.com>
2021-03-25 13:50:20 +0530

    Chart added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import Courses from "./Components/Courses.js";
 import Header from "./Components/Header";
 import MyCourse from "./Components/MyCourse";
 import SideBar from "./Components/SideBar";
+import { SenseiProvider, themeWhite as theme } from "react-sensei";
 import "./Styles/App.css";
+import AreaChart from "./Components/AreaChart.jsx";
 
 function App() {
    return (
-      <div className="App">
-         <SideBar />
-         <div>
-            <Header />
-            <MyCourse />
-            <Courses />
+      <SenseiProvider theme={theme}>
+         <div className="App">
+            <SideBar />
+            <div>
+               <Header />
+               <MyCourse />
+               <Courses />
+            </div>
+            <div className="right-container">
+               <AreaChart />
+            </div>
          </div>
-      </div>
+      </SenseiProvider>
    );
 }
 
